7604da5a7e1bc53d6e8b03453271c2d9012c83ea
Julianf-30 <julian.l.fechner@gmail.com>
2025-10-27 17:26:34 +0000

    made main menu sequence

---
+++ thecode.py
@@ -0,0 +1,23 @@
+import time
+main_menu = ""
+while main_menu != "help" or "continue":
+    print("Hello, welcome to the npc generator, a generator for all your npc needs")
+    time.sleep(1)
+    main_menu = input("Welcome to the main menu, for any questions \ntype help, to continue on to making your npc type continue: ")
+    if main_menu == "help":
+        print("Welcome to the help forum, we hope this will answer any questions you have")
+        print()
+        time.sleep(1)
+        print("This npc generator will allow you to customize the attributes of your npc, or make a random one")
+        time.sleep(4)
+        print()
+        print("You will not be able to see a physical copy of your npcs, just what their attributes are. \n You can choose to make multiple or just one")
+        time.sleep(4)
+        print()
+        print("We hope this answered all of your questions about this generator")
+        help_input = input("Type continue to continue to the generator: ")
+    elif main_menu == "continue":
+        break 
+
+    print("Welcome to the Gnereator section")
+    
